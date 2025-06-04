b801822bb3e0a3c60ba85f19b6c517c3e052c43b
HOLLOW-Ai <m.huynh0320@gmail.com>
2025-06-04 13:09:16 -0400

    Update healthcare_ddl_script.sql

--- healthcare-project/healthcare_ddl_script.sql
+++ healthcare-project/healthcare_ddl_script.sql
@@ -1,6 +1,35 @@
-DROP TABLE IF EXISTS healthcare;
+USE portfolio;
 
-CREATE TABLE healthcare (
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
+
+DROP TABLE IF EXISTS bronze.healthcare;
+
+CREATE TABLE bronze.healthcare (
+	name NVARCHAR(50),
+	age NVARCHAR(50),
+	gender NVARCHAR(50),
+	blood_type NVARCHAR(50),
+	medical_condition NVARCHAR(50),
+	admission_date NVARCHAR(50),
+	doctor NVARCHAR(50),
+	hospital NVARCHAR(50),
+	insurance_provider NVARCHAR(50),
+	billing_amount NVARCHAR(50),
+	room_number NVARCHAR(50),
+	admission_type NVARCHAR(50),
+	discharge_date NVARCHAR(50),
+	medication NVARCHAR(50),
+	test_results NVARCHAR(50)
+);
+
+DROP TABLE IF EXISTS silver.healthcare;
+
+CREATE TABLE silver.healthcare (
 	name NVARCHAR(50),
 	age INT,
 	gender NVARCHAR(10),
@@ -10,13 +39,16 @@ CREATE TABLE healthcare (
 	doctor NVARCHAR(50),
 	hospital NVARCHAR(50),
 	insurance_provider NVARCHAR(50),
-	billing_amount FLOAT,
+	billing_amount DECIMAL(10, 2),
 	room_number INT,
 	admission_type NVARCHAR(25),
 	discharge_date DATE,
 	medication NVARCHAR(50),
 	test_results NVARCHAR(25)
 );
+
+SELECT *
+FROM INFORMATION_SCHEMA.TABLES;
 
 SELECT TABLE_NAME
 FROM INFORMATION_SCHEMA.TABLES;
