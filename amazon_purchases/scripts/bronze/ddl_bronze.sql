e7198f20f2505561029d3026bfaec07d34794130
HOLLOW-Ai <m.huynh0320@gmail.com>
2025-06-09 16:17:22 -0400

    Create ddl_bronze.sql

---
+++ amazon_purchases/scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS bronze.survey_response;
+CREATE TABLE bronze.survey_response (
+	response_id NVARCHAR(255) NOT NULL,
+	q_demos_age_group NVARCHAR(255) NULL,
+	q_demos_hispanic NVARCHAR(255) NULL,
+	q_demos_race NVARCHAR(255) NULL,
+	q_demos_education NVARCHAR(255) NULL,
+	q_demos_income NVARCHAR(255) NULL,
+	q_demos_gender NVARCHAR(255) NULL,
+	q_demos_sexual_orientation NVARCHAR(255) NULL,
+	q_demos_state NVARCHAR(255) NULL,
+	q_amazon_use_howmany NVARCHAR(255) NULL,
+	q_amazon_use_hh_size NVARCHAR(255) NULL,
+	q_amazon_use_how_oft NVARCHAR(255) NULL,
+	q_substance_cig_use NVARCHAR(255) NULL,
+	q_substance_marij_use NVARCHAR(255) NULL,
+	q_substance_alcohol_use NVARCHAR(255) NULL,
+	q_personal_diabetes NVARCHAR(255) NULL,
+	q_personal_wheelchair NVARCHAR(255) NULL,
+	q_life_changes NVARCHAR(255) NULL,
+	q_sell_your_data NVARCHAR(255) NULL,
+	q_sell_consumer_data NVARCHAR(255) NULL,
+	q_small_biz_use NVARCHAR(255) NULL,
+	q_census_use NVARCHAR(255) NULL,
+	q_research_society NVARCHAR(255) NULL
+);
