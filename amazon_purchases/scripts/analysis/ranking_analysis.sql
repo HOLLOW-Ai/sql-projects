665627240b7da1000718461a73bd7ff16d2aa947
HOLLOW-Ai <m.huynh0320@gmail.com>
2025-06-16 18:31:49 -0400

    Update ranking_analysis.sql

--- amazon_purchases/scripts/analysis/ranking_analysis.sql
+++ amazon_purchases/scripts/analysis/ranking_analysis.sql
@@ -9,9 +9,43 @@ Purpose:
 ===============================================================================
 */
 
--- Most popular category per race
--- Top 5 categories per race
--- Do both counting distinct buyers once and then another with ppl ordering multiple times
+-- Top 5 Categories by Total Orders
+WITH ranked_cats AS (
+	SELECT
+		  category
+		, COUNT(response_id) AS num_orders
+		, DENSE_RANK() OVER (ORDER BY COUNT(response_id) DESC) AS rnk
+	FROM silver.amazon_purchases
+	GROUP BY category
+)
+SELECT
+	  category
+	, num_orders
+	, rnk
+FROM ranked_cats
+WHERE rnk <= 5
+ORDER BY rnk
+
+-- Top 5 Categories by Total Sales
+WITH ranked_sales AS (
+	SELECT
+		  category
+		, SUM(purchase_price_per_unit * quantity) AS total_sales
+		, DENSE_RANK() OVER (ORDER BY SUM(purchase_price_per_unit * quantity) DESC) AS rnk
+	FROM silver.amazon_purchases
+	GROUP BY category
+)
+SELECT
+	  category
+	, total_sales
+	, rnk
+FROM ranked_sales
+WHERE rnk <= 5
+ORDER BY rnk
+;
+
+-- Top 5 Categories for each Race
+-- Note: This query ends up counting some of the respondents multiple times because they identified with multiple races
 WITH orders_race AS (
 	SELECT
 		  P.response_id
@@ -42,8 +76,7 @@ WHERE rnks <= 5
 ORDER BY race, rnks
 ;
 
--- Most popular category per age group; age q_id = 1
--- Top 5 Categories by Age Group
+-- Top 5 Categories for each Age Group
 WITH orders_age_groups AS (
 	SELECT
 		  P.response_id
@@ -70,6 +103,48 @@ SELECT
 	, num_orders
 	, rnks AS rank
 FROM categories_rank
-WHERE rnks <= 5
+WHERE rnks <= 5 -- Change if you want to see less categories
 ORDER BY age_group, rnks
 ;
+
+-- Ranking Age Groups by Total Orders
+WITH orders_per_user AS (
+	-- Number of orders made by each respondent
+	SELECT
+		  response_id
+		, COUNT(*) AS num_orders
+	FROM silver.amazon_purchases
+	GROUP BY response_id
+), age_answers AS (
+	-- Respondents' answer to question 16 about wheelchair use
+	SELECT
+		response_id
+		, answer_id
+	FROM silver.user_answers
+	WHERE q_id = 1
+), age_orders AS (
+	-- Combining the 2 previous CTEs
+	SELECT
+		  O.response_id
+		, O.num_orders
+		, A.answer_id
+	FROM orders_per_user O
+	INNER JOIN age_answers A
+		ON O.response_id = A.response_id
+), orders_per_group AS (
+	-- Average orders made by answer to wheelchair question
+	SELECT
+		  answer_id
+		, COUNT(*) AS total_orders
+	FROM age_orders
+	GROUP BY answer_id
+)
+SELECT
+	  answers.answer_text
+	, total_orders
+FROM orders_per_group
+INNER JOIN silver.answers
+	ON orders_per_group.answer_id = answers.answer_id
+ORDER BY total_orders DESC
+;
+-- Perhaps we see a skew because younger people may be more likely to respond to an online survey
