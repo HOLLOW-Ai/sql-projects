775f1b9fa099c7fbad30ba79aa24d714755a9b76
HOLLOW-Ai <m.huynh0320@gmail.com>
2025-08-31 01:13:07 -0400

    Update init_table_creation.sql

--- synthea_covid_data/scripts/synthea_schema/init_table_creation.sql
+++ synthea_covid_data/scripts/synthea_schema/init_table_creation.sql
@@ -29,7 +29,7 @@ CREATE TABLE synthea_schema.allergies (
   severity1    NVARCHAR(255),
   reaction2    NVARCHAR(255),
   description2 NVARCHAR(255),
-  severity2    NVARCHAR(255),
+  severity2    NVARCHAR(255)
 );
 
 
@@ -61,7 +61,7 @@ CREATE TABLE synthea_schema.conditions (
   encounter     NVARCHAR(255), -- FK to Encounters
   system        NVARCHAR(255),
   code          NVARCHAR(255),
-  description   NVARCHAR(255),
+  description   NVARCHAR(255)
 );
 
 
@@ -99,7 +99,7 @@ CREATE TABLE synthea_schema.encounters (
   total_claim_cost		DECIMAL(10, 2),
   payer_coverage		  DECIMAL(10, 2),
   reasoncode   			  NVARCHAR(255),
-  reasondescription   NVARCHAR(255),
+  reasondescription   NVARCHAR(255)
 );
 
 
@@ -138,163 +138,194 @@ CREATE TABLE synthea_schema.immunizations (
 );
 
 
+-- Medications
+IF OBJECT_ID ('synthea_schema.medications', 'U') IS NOT NULL
+  DROP TABLE synthea_schema.medications;
 
---HINT DISTRIBUTE_ON_RANDOM
-create table @synthea_schema.medications (
-"start"         date,
-stop          date,
-patient       varchar(1000),
-payer		varchar(1000),
-encounter     varchar(1000),
-code          varchar(100),
-description   varchar(1000),
-base_cost	  numeric,
-payer_coverage		numeric,
-dispenses			int,
-totalcost			numeric,
-reasoncode   	varchar(100),
-reasondescription   varchar(255)
+CREATE TABLE synthea_schema.medications (
+  "start"             DATETIME,
+  stop                DATETIME,
+  patient             NVARCHAR(255), -- FK to Patient
+  payer		            NVARCHAR(255), -- FK to Payer
+  encounter           NVARCHAR(255), -- FK to Encounter
+  code                NVARCHAR(255),
+  description         NVARCHAR(255),
+  base_cost	          DECIMAL(10, 2),
+  payer_coverage		  DECIMAL(10, 2),
+  dispenses			      INT,
+  totalcost			      DECIMAL(10, 2),
+  reasoncode   	      NVARCHAR(255),
+  reasondescription   NVARCHAR(255)
 );
 
---HINT DISTRIBUTE_ON_RANDOM
+
+-- Observations
+IF OBJECT_ID ('synthea_schema.observations', 'U') IS NOT NULL
+  DROP TABLE synthea_schema.observations;
+
 create table @synthea_schema.observations (
-"date"         date,
-patient       varchar(1000),
-encounter     varchar(1000),
-category      varchar(1000),
-code          varchar(100),
-description   varchar(255),
-value     		varchar(1000),
-units         varchar(100),
-"type"		  	varchar(100)
-);
-
---HINT DISTRIBUTE_ON_RANDOM
-create table @synthea_schema.organizations (
-id			  varchar(1000),
-"name"	      varchar(1000),
-address       varchar(1000),
-city		  varchar(100),
-state     	  varchar(100),
-zip           varchar(100),
-lat		numeric,
-lon 		numeric,
-phone		  varchar(100),
-revenue		numeric,
-utilization	  varchar(100)
-);
-
---HINT DISTRIBUTE_ON_RANDOM
-create table @synthea_schema.patients (
-id            varchar(1000),
-birthdate     date,
-deathdate     date,
-ssn           varchar(100),
-drivers       varchar(100),
-passport      varchar(100),
-prefix        varchar(100),
-first         varchar(100),
-middle         varchar(100),
-last          varchar(100),
-suffix        varchar(100),
-maiden        varchar(100),
-marital       varchar(100),
-race          varchar(100),
-ethnicity     varchar(100),
-gender        varchar(100),
-birthplace    varchar(100),
-address       varchar(100),
-city					varchar(100),
-state					varchar(100),
-county		varchar(100),
-fips varchar(100),
-zip						varchar(100),
-lat		numeric,
-lon		numeric,
-healthcare_expenses	numeric,
-healthcare_coverage	numeric,
-income int
-);
-
---HINT DISTRIBUTE_ON_RANDOM
-create table @synthea_schema.procedures (
-"start"         date,
-stop          date,
-patient       varchar(1000),
-encounter     varchar(1000),
-system          varchar(100),
-code          varchar(100),
-description   varchar(255),
-base_cost		numeric,
-reasoncode	varchar(1000),
-reasondescription	varchar(1000)
-);
-
---HINT DISTRIBUTE_ON_RANDOM
-create table @synthea_schema.providers (
-id varchar(1000),
-organization varchar(1000),
-"name" varchar(100),
-gender varchar(100),
-speciality varchar(100),
-address varchar(255),
-city varchar(100),
-state varchar(100),
-zip varchar(100),
-lat numeric,
-lon numeric,
-encounters int,
-"procedures" int
+  "date"        DATETIME,
+  patient       NVARCHAR(255), -- FK to Patient
+  encounter     NVARCHAR(255), -- FK to Encounter
+  category      NVARCHAR(255),
+  code          NVARCHAR(255),
+  description   NVARCHAR(255),
+  value     		NVARCHAR(255),
+  units         NVARCHAR(255),
+  "type"		  	NVARCHAR(255)
 );
 
 
+-- Organizations
+IF OBJECT_ID ('synthea_schema.organizations', 'U') IS NOT NULL
+  DROP TABLE synthea_schema.organizations;
 
-
-
---HINT DISTRIBUTE_ON_RANDOM
-create table @synthea_schema.payer_transitions (
- patient           varchar(1000),
-  memberid         varchar(1000),
-  start_date       date,
-  end_date         date,
-  payer            varchar(1000),
-  secondary_payer  varchar(1000),
-  plan_ownership        varchar(1000),
-  owner_name       varchar(1000)
+CREATE TABLE synthea_schema.organizations (
+  id			      NVARCHAR(255), -- PK
+  "name"	      NVARCHAR(255),
+  address       NVARCHAR(255),
+  city		      NVARCHAR(255),
+  state     	  NVARCHAR(255),
+  zip           NVARCHAR(255),
+  lat		        DECIMAL(9, 6),
+  lon 		      DECIMAL(9, 6),
+  phone		      NVARCHAR(50),
+  revenue		    DECIMAL(20, 2),
+  utilization	  NVARCHAR(255)
 );
 
---HINT DISTRIBUTE_ON_RANDOM
-create table @synthea_schema.payers (
-  id                       varchar(1000),
-  "name"                     varchar(1000),
-  ownership                varchar NULL,
-  address                  varchar(1000),
-  city                     varchar(1000),
-  state_headquartered      varchar(1000),
-  zip                      varchar(1000),
-  phone                    varchar(1000),
-  amount_covered           numeric,
-  amount_uncovered         numeric,
-  revenue                  numeric,
-  covered_encounters       numeric,
-  uncovered_encounters     numeric,
-  covered_medications      numeric,
-  uncovered_medications    numeric,
-  covered_procedures       numeric,
-  uncovered_procedures     numeric,
-  covered_immunizations    numeric,
-  uncovered_immunizations  numeric,
-  unique_customers         numeric,
-  qols_avg                 numeric,
-  member_months            numeric
+-- Patients
+IF OBJECT_ID ('synthea_schema.patients', 'U') IS NOT NULL
+  DROP TABLE synthea_schema.patients;
+
+CREATE TABLE synthea_schema.patients (
+  id                  NVARCHAR(255), -- PK
+  birthdate           DATE,
+  deathdate           DATE,
+  ssn                 NVARCHAR(255), -- Remove
+  drivers             NVARCHAR(255), -- Remove
+  passport            NVARCHAR(255), -- Is this actually in real healthcare data??
+  prefix              NVARCHAR(255),
+  first               NVARCHAR(255),
+  middle              NVARCHAR(255),
+  last                NVARCHAR(255),
+  suffix              NVARCHAR(255),
+  maiden              NVARCHAR(255),
+  marital             NVARCHAR(255),
+  race                NVARCHAR(255),
+  ethnicity           NVARCHAR(255),
+  gender              NVARCHAR(255),
+  birthplace          NVARCHAR(255),
+  address             NVARCHAR(255),
+  city					      NVARCHAR(255),
+  state					      NVARCHAR(255),
+  county		          NVARCHAR(255),
+  fips                NVARCHAR(255),
+  zip						      NVARCHAR(255),
+  lat		              DECIMAL(9, 6),
+  lon		              DECIMAL(9, 6),
+  healthcare_expenses	DECIMAL(20, 2),
+  healthcare_coverage	DECIMAL(20, 2),
+  income              INT
 );
 
---HINT DISTRIBUTE_ON_RANDOM
-create table @synthea_schema.supplies (
-  "date"       date,
-  patient      varchar(1000),
-  encounter    varchar(1000),
-  code         varchar(1000),
-  description  varchar(1000),
-  quantity     numeric
+
+-- Payers
+IF OBJECT_ID ('synthea_schema.payers', 'U') IS NOT NULL
+  DROP TABLE synthea_schema.payers;
+
+CREATE TABLE synthea_schema.payers (
+  id                       NVARCHAR(255), -- PK
+  "name"                   NVARCHAR(255),
+  ownership                varchar NULL, -- Curious as to why this is NULL, check the CSV later
+  address                  NVARCHAR(255),
+  city                     NVARCHAR(255),
+  state_headquartered      NVARCHAR(255),
+  zip                      NVARCHAR(255),
+  phone                    NVARCHAR(255),
+  amount_covered           DECIMAL(20, 2),
+  amount_uncovered         DECIMAL(20, 2),
+  revenue                  DECIMAL(20, 2),
+  covered_encounters       INT, -- All of these are classified as NUMERIC, but I'm going to be changing it to INT because presumably, it shouldn't be a decimal
+  uncovered_encounters     INT,
+  covered_medications      INT,
+  uncovered_medications    INT,
+  covered_procedures       INT,
+  uncovered_procedures     INT,
+  covered_immunizations    INT,
+  uncovered_immunizations  INT,
+  unique_customers         INT,
+  qols_avg                 DECIMAL(5, 2), -- Check the CSV later
+  member_months            INT -- Check CSV later
+);
+
+
+-- Payer Transitions
+-- I honestly imagine I'm not going to be using this table, tbh
+IF OBJECT_ID ('synthea_schema.payer_transitions', 'U') IS NOT NULL
+  DROP TABLE synthea_schema.payer_transitions;
+
+CREATE TABLE synthea_schema.payer_transitions (
+  patient          NVARCHAR(255), -- FK to Patient
+  memberid         NVARCHAR(255),
+  start_date       DATE,
+  end_date         DATE,
+  payer            NVARCHAR(255), -- FK to Payers
+  secondary_payer  NVARCHAR(255), -- Optional FK to Payers
+  plan_ownership   NVARCHAR(255),
+  owner_name       NVARCHAR(255)
+);
+
+
+-- Procedures
+IF OBJECT_ID ('synthea_schema.procedures', 'U') IS NOT NULL
+  DROP TABLE synthea_schema.procedures;
+
+CREATE TABLE synthea_schema.procedures (
+  "start"           DATETIME,
+  stop              DATETIME,
+  patient           NVARCHAR(255), -- FK to Patient
+  encounter         NVARCHAR(255), -- FK to Encounter
+  system            NVARCHAR(255),
+  code              NVARCHAR(255),
+  description       NVARCHAR(255),
+  base_cost		      DECIMAL(10, 2),
+  reasoncode	      NVARCHAR(255),
+  reasondescription	NVARCHAR(255)
+);
+
+
+-- Providers
+IF OBJECT_ID ('synthea_schema.providers', 'U') IS NOT NULL
+  DROP TABLE synthea_schema.providers;
+
+CREATE TABLE synthea_schema.providers (
+  id             NVARCHAR(255), -- PK
+  organization   NVARCHAR(255), -- FK to Organization
+  "name"         NVARCHAR(255),
+  gender         NVARCHAR(255),
+  speciality     NVARCHAR(255),
+  address        NVARCHAR(255),
+  city           NVARCHAR(255),
+  state          NVARCHAR(255),
+  zip            NVARCHAR(255),
+  lat            DECIMAL(9, 6),
+  lon            DECIMAL(9, 6),
+  encounters     INT,
+  "procedures"   INT
+);
+
+
+-- Supplies
+-- I don't imagine I'll be using this table
+IF OBJECT_ID ('synthea_schema.supplies', 'U') IS NOT NULL
+  DROP TABLE synthea_schema.supplies;
+
+CREATE TABLE synthea_schema.supplies (
+  "date"       DATE,
+  patient      NVARCHAR(255), -- FK to Patient
+  encounter    NVARCHAR(255), -- FK to Encounter
+  code         NVARCHAR(255),
+  description  NVARCHAR(255),
+  quantity     INT -- I assume this to be a whole number, but check CSV anyway
 );
