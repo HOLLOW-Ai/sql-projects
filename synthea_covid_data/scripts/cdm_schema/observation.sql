5bed8158a0a8072439f3c03a46503fc77f399e24
HOLLOW-Ai <m.huynh0320@gmail.com>
2025-09-01 23:54:53 -0400

    Create observation.sql

---
+++ synthea_covid_data/scripts/cdm_schema/observation.sql
@@ -0,0 +1,26 @@
+IF OBJECT_ID ('cdm_schema.observation', 'U') IS NOT NULL
+  DROP TABLE cdm_schema.observation;
+
+CREATE TABLE cdm_schema.observation (
+  observation_id INT, -- Autogen
+  person_id INT,
+  -- observation_concept_id INT,
+  observation_date DATE,
+  observation_datetime DATETIME,
+  observation_type_concept_id INT DEFAULT 38000280,
+  value_as_number DECIMAL(6, 2), -- Check later
+  value_as_string NVARCHAR(60),
+  value_as_concept_id INT DEFAULT 0,
+  qualifier_concept_id INT DEFAULT 0,
+  unit_concept_id INT DEFAULT 0,
+  provider_id INT,
+  visit_occurrence_id INT,
+  visit_detail_id INT,
+  observation_source_value NVARCHAR(50),
+  -- observation_source_concept_id INT,
+  unit_source_value NVARCHAR(50),
+  qualifier_source_value NVARCHAR(50),
+  value_source_value NVARCHAR(50),
+  observation_event_id INT, -- Or Bigint?
+  obs_event_field_concept_id INT
+);
