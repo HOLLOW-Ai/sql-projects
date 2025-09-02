63c83879cd579eadbd538e7370412e2c5e024b08
HOLLOW-Ai <m.huynh0320@gmail.com>
2025-09-02 16:31:27 -0400

    Update measurement.sql

--- synthea_covid_data/scripts/cdm_schema/measurement.sql
+++ synthea_covid_data/scripts/cdm_schema/measurement.sql
@@ -2,19 +2,19 @@ IF OBJECT_ID ('cdm_schema.measurement', 'U') IS NOT NULL
   DROP TABLE cdm_schema.measurement;
 
 CREATE TABLE cdm_schema.measurement (
-  measurement_id INT, -- Autogen
-  person_id INT,
+  measurement_id INT IDENTITY(1, 1) NOT NULL, -- Autogen
+  person_id INT NOT NULL,
   -- measurement_concept_id INT, -- No vocab table
   measurement_date DATE,
   measurement_datetime DATETIME,
   measurement_time NVARCHAR(10),
   measurement_type_concept_id INT,
   operator_concept_id INT,
-  value_as_number DECIMAL(5, 2), -- Value to be decided
+  value_as_number FLOAT, -- Value to be decided
   value_as_concept_id INT,
   unit_concept_id INT,
-  range_low DECIMAL(5, 2),
-  range_high DECIMAL(5, 2),
+  range_low FLOAT,
+  range_high FLOAT,
   provider_id INT,
   visit_occurrence_id INT,
   visit_detail_id INT,
