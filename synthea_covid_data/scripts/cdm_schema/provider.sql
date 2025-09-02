a0268f32c4865c0e0a826f75a0d7534fae08cc37
HOLLOW-Ai <m.huynh0320@gmail.com>
2025-09-02 00:51:45 -0400

    Create provider.sql

---
+++ synthea_covid_data/scripts/cdm_schema/provider.sql
@@ -0,0 +1,18 @@
+IF OBJECT_ID ('cdm_schema.provider', 'U') IS NOT NULL
+  DROP TABLE cdm_schema.provider;
+
+CREATE TABLE cdm_schema.provider (
+  provider_id INT, -- Autogen
+  provider_name NVARCHAR(255),
+  npi NVARCHAR(20),
+  dea NVARCHAR(20),
+  specialty_concept_id INT,
+  care_site_id INT,
+  year_of_birth INT,
+  gender_concept_id INT,
+  provider_source_value NVARCHAR(50),
+  specialty_source_value NVARCHAR(50),
+  specialty_source_concept_id INT,
+  gender_source_value NVARCHAR(50),
+  gender_source_concept_id INT
+);
