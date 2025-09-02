802fa4be34435ffd06bc7bea4b5aa0ce568be8a0
HOLLOW-Ai <m.huynh0320@gmail.com>
2025-09-02 17:50:48 -0400

    Update payer_plan_period.sql

--- synthea_covid_data/scripts/cdm_schema/payer_plan_period.sql
+++ synthea_covid_data/scripts/cdm_schema/payer_plan_period.sql
@@ -2,8 +2,8 @@ IF OBJECT_ID ('cdm_schema.payer_plan_period', 'U') IS NOT NULL
   DROP TABLE cdm_schema.payer_plan_period;
 
 CREATE TABLE cdm_schema.payer_plan_period (
-  payer_plan_period INT,
-  person_id INT,
+  payer_plan_period INT NOT NULL, -- Presumably supposed to be patient_id + payer transition start year
+  person_id INT NOR NULL,
   payer_plan_period_start_date DATE,
   payer_plan_period_end_date DATE,
   payer_concept_id INT DEFAULT 0,
