01caf58041cd518642cf6e6f31687f8ef1cce373
HOLLOW-Ai <m.huynh0320@gmail.com>
2025-09-02 16:41:14 -0400

    Update death.sql

--- synthea_covid_data/scripts/cdm_schema/death.sql
+++ synthea_covid_data/scripts/cdm_schema/death.sql
@@ -2,11 +2,11 @@ IF OBJECT_ID ('cdm_schema.death', 'U') IS NOT NULL
   DROP TABLE cdm_schema.death;
 
 CREATE TABLE cdm_schema.death (
-  person_id INT,
-  death_date DATE,
+  person_id INT NOT NULL,
+  death_date DATE NOT NULL,
   death_datetime DATETIME,
   death_type_concept_id INT DEFEAULT 32817,
   -- cause_concept_id INT
-  cause_source_value INT
+  cause_source_value NVARCHAR(50)
   -- cause_source_concept_id
 );
