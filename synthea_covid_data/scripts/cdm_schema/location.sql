796cb1a6f78ceed8b7a144a4907d9fb527159fbe
HOLLOW-Ai <m.huynh0320@gmail.com>
2025-09-01 20:51:02 -0400

    Create location.sql

---
+++ synthea_covid_data/scripts/cdm_schema/location.sql
@@ -0,0 +1,14 @@
+IF OBJECT_ID ('cdm_schema.location', 'U') IS NOT NULL
+  DROP TABLE cdm_schema.location;
+
+CREATE TABLE cdm_schema.location (
+  location_id INT, -- Autogen
+  address_1 NVARCHAR(50),
+  address_2 NVARCHAR(50),
+  city NVARCHAR(50),
+  "state" NVARCHAR(2),
+  county NVARCHAR(20),
+  zip NVARCHAR(9),
+  location_source_value NVARCHAR(50)
+  -- This example doesn't include Latitude and Longitude on the Github page
+);
