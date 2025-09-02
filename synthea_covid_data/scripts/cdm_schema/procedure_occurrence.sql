81e9be3435cf9b42d20d959497564498bd254b36
HOLLOW-Ai <m.huynh0320@gmail.com>
2025-09-02 16:25:47 -0400

    Update procedure_occurrence.sql

--- synthea_covid_data/scripts/cdm_schema/procedure_occurrence.sql
+++ synthea_covid_data/scripts/cdm_schema/procedure_occurrence.sql
@@ -2,14 +2,14 @@ IF OBJECT_ID ('cdm_schema.procedure_occurrence', 'U') IS NOT NULL
   DROP TABLE cdm_schema.procedure_occurrence;
 
 CREATE TABLE cdm_schema.procedure_occurrence (
-  procedure_occurrence_id INT,
-  person_id INT,
+  procedure_occurrence_id INT NOT NULL,
+  person_id INT NOT NULL,
   -- procedure_concept_id INT, -- No vocab
   procedure_start_date DATE,
   procedure_start_datetime DATETIME,
   procedure_end_date DATE,
   procedure_end_datetime DATETIME,
-  procedure_type_concept_id INT,
+  procedure_type_concept_id INT NOT NULL,
   modifier_concept_id INT,
   quantity INT,
   provider_id INT,
