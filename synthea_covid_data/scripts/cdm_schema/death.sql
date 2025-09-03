9197da72bbbb1096fae64036a6e65f8c40c90a73
HOLLOW-Ai <m.huynh0320@gmail.com>
2025-09-02 20:12:28 -0400

    Update death.sql

--- synthea_covid_data/scripts/cdm_schema/death.sql
+++ synthea_covid_data/scripts/cdm_schema/death.sql
@@ -10,3 +10,24 @@ CREATE TABLE cdm_schema.death (
   cause_source_value NVARCHAR(50)
   -- cause_source_concept_id
 );
+
+
+-- Person needs to be created first
+INSERT INTO cdm_schema.death (
+    person_id
+  , death_date
+  , death_datetime
+  , death_type_concept_id
+  , cause_source_value
+)
+SELECT
+    P.person_id -- person_id
+  , E.start  -- death_date
+  , E.start  -- death_datetime
+  , 32817  -- death_type_concept_id
+  , E.reasoncode  -- cause_source_value
+FROM cdm_schema.person AS P
+INNER JOIN synthea_schema.encounters AS E
+  ON P.person_source_value = E.patient
+WHERE E.code = '308646001'
+;
