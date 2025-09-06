7c8ac813ad341690469421a2f0bd6df77cb32066
HOLLOW-Ai <m.huynh0320@gmail.com>
2025-09-05 21:07:23 -0400

    Update working.sql

--- seattle_libraries/working.sql
+++ seattle_libraries/working.sql
@@ -1,242 +1,315 @@
-CREATE DATABASE library;
 USE library;
-
-DROP TABLE staging;
-
-CREATE TABLE staging (
-	  bibnum NVARCHAR(MAX)
-	, title NVARCHAR(MAX)
-	, author NVARCHAR(MAX)
-	, isbn NVARCHAR(MAX)
-	, pub_year NVARCHAR(MAX)
-	, publisher NVARCHAR(MAX)
-	, item_type NVARCHAR(MAX)
-	, item_col NVARCHAR(MAX)
-	, item_loc NVARCHAR(MAX)
-);
-
-CREATE TABLE loading (
-	  bibnum INT
-	, title NVARCHAR(MAX)
-	, author NVARCHAR(MAX)
-	, isbn NVARCHAR(MAX)
-	, pub_year NVARCHAR(MAX)
-	, publisher NVARCHAR(MAX)
-	, item_type NVARCHAR(MAX)
-	, item_col NVARCHAR(MAX)
-	, item_loc NVARCHAR(MAX)
-);
-
-BULK INSERT staging
-FROM '\Library_Collection_Inventory_20250903(1).csv'
-WITH (
-		FIELDTERMINATOR = ',',
-		ROWTERMINATOR = '0x0A',
-		FIRSTROW = 2,
-		FORMAT = 'CSV' -- Needed to remove the quotes
-);
-
-SELECT TOP 100 *
-FROM staging;
-
-WITH cte AS (
-SELECT bibnum
-FROM staging
-GROUP BY bibnum, title, author, isbn, pub_year, publisher, item_type, item_col, item_loc
-HAVING COUNT(*) > 1
-)
-SELECT COUNT(*)
-FROM cte;
-
-WITH cte2 AS (
-	SELECT DISTINCT *
-	FROM staging
-)
-SELECT COUNT(*)
-FROM cte2;
-
-
-
-INSERT INTO loading
-SELECT DISTINCT *
-FROM staging;
-
-SELECT TOP 100 *, 2025 AS data_year
-FROM loading;
-
--- Added data
--- 2025
-
-
--- Staging Data
-	-- 2024: 14,446,679 rows raw; 1,735,955 distinct
-	-- 2023: 10,180,283 rows raw; 1,807,218 distinct
-	-- 2022: 17,639,430 rows raw; 1,861,674 distinct
-	-- 2021: 17,725,364 rows raw; 1,665,362 distinct
-	-- 2020: 17,487,259 rows raw; 1,661,134 distinct
-
-WITH distinct_cnt AS (
-	SELECT DISTINCT *
-	FROM staging
-)
-SELECT COUNT(*)
-FROM distinct_cnt
-;
-
-
--- Loading
-INSERT INTO loading (
-	  bibnum
-	, title
-	, author
-	, isbn
-	, pub_year
-	, publisher
-	, item_type
-	, item_col
-	, item_loc
-)
-SELECT DISTINCT
-	  bibnum
-	, title
-	, author
-	, isbn
-	, pub_year
-	, publisher
-	, item_type
-	, item_col
-	, item_loc
-FROM staging
-;
-
-SELECT COUNT(*)
-FROM loading;
-
--- Data
--- Without 2020 data, 2,739,958 distinct rows
-WITH distinct_load AS (
-	SELECT DISTINCT *
-	FROM loading
-)
-SELECT COUNT(*)
-FROM distinct_load
-;
-
--- Delete Duplicate Rows from 'loading'
-WITH cte_del AS (
-SELECT
-	  *
-	, ROW_NUMBER() OVER (PARTITION BY bibnum, title, author, isbn, pub_year, publisher, item_type, item_col, item_loc ORDER BY bibnum) AS rn
-FROM loading
-)
-DELETE cte_del
-WHERE rn > 1
-;
-
-
-SELECT TOP 100 *
-FROM loading
-;
-
--- 375,722 rows returned total
-SELECT TOP 10
-	bibnum
-	, COUNT(*) AS num
-FROM loading
-GROUP BY bibnum
-HAVING COUNT(*) > 1
-;
-
--- Differences arise from item_col and item_loc
-SELECT *
-FROM loading
-WHERE bibnum IN (3511456, 3156174, 1736578, 3171494, 3519116, 2808552, 3731922, 3384300)
-ORDER BY bibnum, item_type, item_col, item_loc
-;
-
-/*
-	Info on columns
-	=====================================
-
-	ItemCollection (item_col): Collection code for this item. Value descriptions can be found in Integrated LIbrary System (ILS) Data Dictionary
-
-	ItemLocation (item_loc): Location that owned the item at the time of snapshot. 3-letter code. This can be changed depending on where the item is returned.
-*/
-
-/*
-	ILS Data Dictionary
-	=======================
-	To Do:
-		- There's a row for each ItemCollection, ItemType, Location codes
-		- Need to separate it into tables and Unpivot
-*/
-
-USE library;
-
-CREATE SCHEMA bronze;
-
-SELECT COUNT(*)
-FROM staging;
-
-DROP TABLE staging;
-
-CREATE TABLE bronze.dictionary (
-	  code NVARCHAR(50)
-	, description NVARCHAR(255)
-	, code_type NVARCHAR(50)
-	, format_group NVARCHAR(50)
-	, format_subgroup NVARCHAR(50)
-	, cat_group NVARCHAR(50)
-	, cat_subgroup NVARCHAR(50)
-	, age_group NVARCHAR(25)
-);
-
-BULK INSERT bronze.dictionary
-FROM '\Integrated_Library_System_(ILS)_Data_Dictionary_20250902.csv'
-WITH (
-		FIELDTERMINATOR = ',',
-		ROWTERMINATOR = '0x0A',
-		FIRSTROW = 2,
-		FORMAT = 'CSV' -- Needed to remove the quotes
-);
-
-SELECT *
-FROM bronze.dictionary;
-
-
-DROP TABLE bronze.checkouts;
-
-CREATE TABLE bronze.checkouts (
-	  id NVARCHAR(MAX)
-	, checkout_year INT
-	, bibnum INT
-	, item_type NVARCHAR(MAX)
-	, collection NVARCHAR(MAX)
-	, item_title NVARCHAR(MAX)
-	, checkout_datetime DATETIME
-);
-
-BULK INSERT bronze.checkouts
-FROM 'Checkouts_By_Title_(Physical_Items)_20250904.csv'
-WITH (
-		FIELDTERMINATOR = ',',
-		ROWTERMINATOR = '0x0A',
-		FIRSTROW = 2,
-		FORMAT = 'CSV' -- Needed to remove the quotes
-);
-
-SELECT TOP 50 *
-FROM bronze.checkouts;
 
 SELECT *
 FROM INFORMATION_SCHEMA.TABLES;
 
-/*
-	=================================
-	Data Check for bronze.dictionary
-	=================================
 
-	CREATE TABLE silver.dictionary (
+/*  =====================
+	Data Dictionary Table
+    =====================
+	- Remove 'Location' rows from table
+	- Create 2 tables for ItemCollection and ItemType
+	- ItemTypeDetail: HOTSPOT? HOTSPOT rows found in bronze.checkouts, just remove from checkouts when loading to silver
+	- This table is relatively clean
+*/
+
+SELECT TOP 10 *
+FROM bronze.dictionary
+WHERE code like '%HOTSPOT%';
+
+SELECT TOP 10 *
+FROM bronze.checkouts
+WHERE item_title LIKE '%HOTSPOT%';
+
+SELECT *
+FROM bronze.dictionary;
+
+SELECT DISTINCT code_type
+FROM bronze.dictionary;
+
+SELECT *
+FROM bronze.dictionary
+WHERE code_type = 'ItemTypeDetail';
+
+WITH item_col_tbl AS (
+	SELECT *
+	FROM bronze.dictionary
+	WHERE code_type = 'ItemCollection'
+)
+SELECT *
+FROM item_col_tbl
+WHERE LOWER(code) != LOWER(TRIM(code))
+;
+
+-- Probably drop the 'Location' code_type because I don't plan to keep the item location in the Library Collection table
+WITH item_col_tbl AS (
+	SELECT *
+	FROM bronze.dictionary
+	WHERE code_type = 'Location'
+)
+SELECT *
+FROM item_col_tbl
+WHERE code = 'cen'
+;
+
+-- Keep description as is
+WITH item_col_tbl AS (
+	SELECT *
+	FROM bronze.dictionary
+	WHERE code_type != 'Location'
+)
+SELECT
+	description
+	, COUNT(*)
+FROM item_col_tbl
+GROUP BY description
+HAVING COUNT(*) > 1
+;
+
+--SELECT
+--	  code
+--	, description
+--	, format_group
+--	, format_subgroup
+--	, cat_group
+--	, cat_subgroup
+--	, age_group
+--FROM item_col_tbl;
+
+
+/*  =====================
+	Catalog Table
+    =====================
+	- Should I make a publisher table?
+	- Some publishers end with commas at the end and some have "" around them - remove if possible; periods are fine
+	- No difference with UPPER() or LOWER(), mainly the spelling difference
+	- Possible Change:
+		- Keep multiple of the same copy of a book, but they belong to a different collection and/or location
+		- Add in a new surrogate key like ROW_NUMBER() to the Inventory table; Call it inventory_id
+		- Get the new Key into the Checkouts table by joining on BibNumber and Collection
+	- Some of the titles and authors have inconsistencies, but just join on bibnum; get rid of dupes using ROW_NUMBER() and CTE
+	- Do TRIM(), LOWER() on the titles and author if necessary
+	- Split author name by delimiter
+	- Drop Location, but keep the Collection
+	- Order by ISBN and get rid of duplicates by ROW_NUMBER() OVER (PARTITION BY bibnum, item_type, item_col ORDER BY isbn DESC)
+	
+*/
+
+SELECT TOP 100
+	  bibnum
+	, title
+	, author
+	, isbn
+	, pub_year
+	, publisher
+	, item_type
+	, item_col
+FROM loading
+;
+
+-- Normal: 126,559 rows
+-- Inconsistencies when inputting Publisher name, weird quotation marks around some of them
+SELECT
+	  LOWER(TRIM(publisher))
+	, COUNT(*)
+FROM loading
+GROUP BY LOWER(TRIM(publisher))
+ORDER BY LOWER(TRIM(publisher))
+;
+
+-- Update so publisher name doesn't end with a comma, period is fine
+SELECT DISTINCT publisher
+FROM loading
+WHERE publisher LIKE '%,'
+;
+
+
+SELECT TOP 100 *
+FROM loading
+WHERE bibnum IN (
+	SELECT
+		bibnum
+	FROM loading
+	GROUP BY bibnum
+	HAVING COUNT(*) > 1
+)
+ORDER BY bibnum
+;
+
+-- Multiple rows where you have multiple bibnum with multiple isbn
+
+WITH bibnum_isbn AS (
+SELECT DISTINCT
+	bibnum
+	, isbn
+FROM loading
+), cte2 AS (
+	SELECT bibnum, COUNT(*) AS cnt
+	FROM bibnum_isbn
+	GROUP BY bibnum
+	HAVING COUNT(*) > 1
+)
+SELECT *
+FROM cte2;
+--SELECT DISTINCT TOP 100 bibnum, isbn
+--FROM loading
+--WHERE bibnum IN (SELECT bibnum FROM cte2)
+--ORDER BY bibnum;
+
+-- Why does Suzume 1 and Suzume 3 have the same bibnum??
+SELECT *, ROW_NUMBER() OVER (PARTITION BY bibnum, item_type, item_col ORDER BY isbn DESC) AS rn
+FROM loading
+WHERE bibnum IN (1702, 8489, 4031834, 3470, 8157, 11614, 13917, 19786)
+;
+
+-- Looks like someone incorrectly put Suzume 3 with the same bibnum as Suzume 1 in April 2025
+
+SELECT *
+FROM loading
+WHERE author = 'Shinkai, Makoto'
+ORDER BY bibnum
+;
+
+SELECT DISTINCT bibnum, author, isbn
+FROM loading
+WHERE bibnum = 4031834
+;
+
+-- ISBN null, item_col and item_loc differ
+-- Could indicate multiple copies of this book existing in the inventory
+SELECT *, ROW_NUMBER() OVER (PARTITION BY bibnum, item_type, item_col ORDER BY isbn DESC)
+FROM loading
+WHERE bibnum = 1702
+;
+
+SELECT DISTINCT *
+FROM loading
+WHERE bibnum = 3470
+ORDER BY isbn DESC
+;
+
+-- Spelling diff of title, isbn has 1+, pub_year weird format, publisher spelling diff
+SELECT DISTINCT *
+FROM loading
+WHERE bibnum = 8157
+;
+
+-- Spelling diff of Author
+SELECT DISTINCT *
+FROM loading
+WHERE bibnum = 19786
+;
+
+SELECT *, ROW_NUMBER() OVER (PARTITION BY bibnum, isbn, item_type, item_col ORDER BY isbn DESC) AS rn
+FROM loading
+WHERE bibnum IN (1702, 8489, 4031834, 3470, 8157, 11614, 13917, 19786)
+;
+
+
+
+/*  =====================
+	Checkouts Table
+    =====================
+	- 
+	
+*/
+
+SELECT TOP 10 *
+FROM bronze.checkouts;
+
+-- Multiple id's show up
+SELECT TOP 5 id
+FROM bronze.checkouts
+GROUP BY id
+HAVING COUNT(*) > 1
+;
+
+SELECT *
+FROM bronze.checkouts
+WHERE id IN ('202303290802000010107757345', '202303290810000010104848691', '202303290937000010102444055', '202303290939000010093342136', '202303290939000010107019035')
+ORDER BY id
+;
+
+-- 16,938,644 vs 16,614,613
+SELECT COUNT(*), COUNT(DISTINCT id)
+FROM bronze.checkouts;
+
+-- 16,146,613 records return, remove when moving to new table
+WITH distinct_recs AS (
+	SELECT DISTINCT *
+	FROM bronze.checkouts
+)
+SELECT COUNT(*)
+FROM distinct_recs
+;
+
+SELECT *
+FROM bronze.checkouts
+WHERE LOWER(item_title) != LOWER(TRIM(item_title))
+;
+
+SELECT *
+FROM bronze.checkouts
+WHERE LEN(item_title) != LEN(TRIM(item_title))
+;
+
+SELECT TOP 10 bibnum
+FROM bronze.checkouts;
+
+CREATE SCHEMA silver;
+
+
+-- Load checkouts into the Silver schema
+WITH distinct_recs AS (
+	SELECT DISTINCT *
+	FROM bronze.checkouts
+)
+SELECT
+	  id
+	, checkout_year
+	, bibnum
+	, item_type
+	, collection
+	, item_title
+	, checkout_datetime
+INTO silver.checkouts
+FROM distinct_recs;
+
+-- Dropping it for the consideration of my storage
+DROP TABLE bronze.checkouts;
+
+CREATE TABLE silver.checkout_records (
+		  id NVARCHAR(50)
+		, checkout_year INT
+		, bibnum INT
+		, item_type NVARCHAR(10)
+		, collection NVARCHAR(10)
+		, item_title NVARCHAR(400)
+		, checkout_datetime DATETIME
+);
+
+INSERT INTO silver.checkout_records (id, checkout_year, bibnum, item_type, collection, item_title, checkout_datetime)
+SELECT id, checkout_year, bibnum, item_type, collection, item_title, checkout_datetime
+FROM silver.checkouts;
+
+DROP TABLE silver.checkouts;
+
+SELECT COUNT(*)
+FROM silver.checkout_records;
+
+SELECT *
+FROM silver.checkout_records
+WHERE item_title = 'HOTSPOT'
+;
+
+-- Deleting the hotspot records from checkouts
+DELETE FROM silver.checkout_records
+WHERE item_title = 'HOTSPOT'
+;
+
+
+
+CREATE TABLE silver.dictionary (
 	  code NVARCHAR(20)
 	, description NVARCHAR(100)
 	, code_type NVARCHAR(25)
@@ -245,102 +318,43 @@ FROM INFORMATION_SCHEMA.TABLES;
 	, cat_group NVARCHAR(25)
 	, cat_subgroup NVARCHAR(20)
 	, age_group NVARCHAR(20)
-	);
+);
 
-*/
-
+WITH cte_dictionary AS (
+	SELECT 
+		  code
+		, description
+		, code_type
+		, format_group
+		, format_subgroup
+		, cat_group
+		, cat_subgroup
+		, age_group
+	FROM bronze.dictionary
+	WHERE code NOT LIKE '%HOTSPOT%'
+)
+INSERT INTO silver.dictionary (
+	  code
+	, description
+	, code_type
+	, format_group
+	, format_subgroup
+	, cat_group
+	, cat_subgroup
+	, age_group
+)
 SELECT
-	  MAX(LEN(code))
-	, MAX(LEN(description))
-	, MAX(LEN(code_type))
-	, MAX(LEN(format_group))
-	, MAX(LEN(format_subgroup))
-	, MAX(LEN(cat_group))
-	, MAX(LEN(cat_subgroup))
-	, MAX(LEN(age_group))
-FROM bronze.dictionary;
+	  code
+	, description
+	, code_type
+	, format_group
+	, format_subgroup
+	, cat_group
+	, cat_subgroup
+	, age_group
+FROM cte_dictionary
+;
 
-SELECT DISTINCT age_group
-FROM bronze.dictionary;
-
-
-/*
-	=================================
-	Data Check for bronze.checkouts
-	=================================
-
-	CREATE TABLE silver.checkouts (
-		  id NVARCHAR(50)
-		, checkout_year INT
-		, bibnum INT
-		, item_type NVARCHAR(10)
-		, collection NVARCHAR(10)
-		, item_title NVARCHAR(400)
-		, checkout_datetime DATETIME
-	);
-
-*/
-
-SELECT
-	  MAX(LEN(id))
-	, MAX(LEN(checkout_year))
-	, MAX(LEN(bibnum))
-	, MAX(LEN(item_type))
-	, MAX(LEN(collection))
-	, MAX(LEN(item_title))
-	, MAX(LEN(checkout_datetime))
-FROM bronze.checkouts;
-
-SELECT TOP 10 *
-FROM loading;
-
-
-
-/*
-	=================================
-	Data Check for loading (catalog of books that have been recorded in inventory)
-	=================================
-
-	CREATE TABLE loading (
-		  bibnum INT
-		, title NVARCHAR(3000)
-		, author NVARCHAR(500)
-		, isbn NVARCHAR(200)
-		, pub_year NVARCHAR(500)
-		, publisher NVARCHAR(500)
-		, item_type NVARCHAR(10)
-		, item_col NVARCHAR(10)
-		, item_loc NVARCHAR(10)
-	);
-
-*/
-
-SELECT
-	  MAX(LEN(bibnum))
-	, MAX(LEN(title))
-	, MAX(LEN(author))
-	, MAX(LEN(isbn))
-	, MAX(LEN(pub_year))
-	, MAX(LEN(publisher))
-	, MAX(LEN(item_type))
-	, MAX(LEN(item_col))
-	, MAX(LEN(item_loc))
-FROM loading;
-
-/*
-	===================================
-	To Do
-	===================================
-
-	Data Cleaning
-	- Make sure no white spaces (TRIM)
-	- Make sure the necessary columns have an ID; no NULLs
-
-	After Cleaning
-	- Load into Silver layer, with necessary adjustments to table creation (rename columns, change sizes of values, remove checkout_year?)
-	- Create Gold layer
-	- Plan out how to unpivot the Data Dictionary table
-	- Clear out Bronze layer because of storage reasons
-	- Create columns necessary for analysis
-	- If needed, export Silver layer tables to CSV if storage runs out
-*/
+SELECT *
+FROM silver.dictionary
+WHERE code like '%HOTSPOT%';
