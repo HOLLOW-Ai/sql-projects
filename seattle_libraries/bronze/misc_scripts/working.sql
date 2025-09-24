2ac1a7563a882c273134373a39eb244b9e76c67a
HOLLOW-Ai <m.huynh0320@gmail.com>
2025-09-24 17:23:46 -0400

    Update working.sql

--- seattle_libraries/bronze/misc_scripts/working.sql
+++ seattle_libraries/bronze/misc_scripts/working.sql
@@ -1,7 +1,8 @@
 /*
 ===========================================================
-  If you're reading this, these are just random snippets
-  of SQL queries I used while working in the Bronze
-  layer.
+  Miscellaneous Scripts
 ===========================================================
+  If you're reading this, these are just random snippets of SQL queries I used while working in the Bronze layer.
+
+  I didn't think it fit in the other folders so it was to keep the clean scripts separate from the mess of my working queries.
 */
