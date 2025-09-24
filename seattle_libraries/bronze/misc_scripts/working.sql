0bcdd21c2dfdb08bc7f4a9b52d4a64e67127e05e
HOLLOW-Ai <m.huynh0320@gmail.com>
2025-09-24 17:22:20 -0400

    Create working.sql

---
+++ seattle_libraries/bronze/misc_scripts/working.sql
@@ -0,0 +1,7 @@
+/*
+===========================================================
+  If you're reading this, these are just random snippets
+  of SQL queries I used while working in the Bronze
+  layer.
+===========================================================
+*/
