66dc88e475ba382101ebe41b88f43019368ccb4b
HOLLOW-Ai <m.huynh0320@gmail.com>
2025-09-24 16:01:20 -0400

    Update creation.sql

--- seattle_libraries/silver/creation.sql
+++ seattle_libraries/silver/creation.sql
@@ -1,1 +1,9 @@
---filler
+CREATE TABLE silver.checkout_records (
+		  id NVARCHAR(50)    -- BIGINT doesn't work because value overflows
+		, checkout_year INT
+		, bibnum INT
+		, item_type NVARCHAR(10)
+		, collection NVARCHAR(10)
+		, item_title NVARCHAR(400)
+		, checkout_datetime DATETIME
+);
