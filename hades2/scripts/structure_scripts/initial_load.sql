12a84b05765362f7aa925e1762a6de6987d22a2c
HOLLOW-Ai <m.huynh0320@gmail.com>
2025-10-23 16:04:54 -0400

    Update initial_load.sql

--- hades2/scripts/structure_scripts/initial_load.sql
+++ hades2/scripts/structure_scripts/initial_load.sql
@@ -2,52 +2,46 @@ BULK INSERT bronze.run_log
 FROM '[Input Path]'
 WITH (
 		FIELDTERMINATOR = ',',
-		ROWTERMINATOR = '0x0A', -- Should check in Notepad++
-		FIRSTROW = 2,
-		FORMAT = 'CSV' -- Needed to remove the quotes
+		ROWTERMINATOR = '\n',
+		FIRSTROW = 2
 );
 
 BULK INSERT bronze.keepsake_log
 FROM '[Input Path]'
 WITH (
 		FIELDTERMINATOR = ',',
-		ROWTERMINATOR = '0x0A', -- Should check in Notepad++
-		FIRSTROW = 2,
-		FORMAT = 'CSV' -- Needed to remove the quotes
+		ROWTERMINATOR = '\n',
+		FIRSTROW = 2
 );
 
 BULK INSERT bronze.weapon_log
 FROM '[Input Path]'
 WITH (
 		FIELDTERMINATOR = ',',
-		ROWTERMINATOR = '0x0A', -- Should check in Notepad++
-		FIRSTROW = 2,
-		FORMAT = 'CSV' -- Needed to remove the quotes
+		ROWTERMINATOR = '\n',
+		FIRSTROW = 2
 );
 
 BULK INSERT bronze.vow_log
 FROM '[Input Path]'
 WITH (
 		FIELDTERMINATOR = ',',
-		ROWTERMINATOR = '0x0A', -- Should check in Notepad++
-		FIRSTROW = 2,
-		FORMAT = 'CSV' -- Needed to remove the quotes
+		ROWTERMINATOR = '\n',
+		FIRSTROW = 2
 );
 
 BULK INSERT bronze.arcana_log
 FROM '[Input Path]'
 WITH (
 		FIELDTERMINATOR = ',',
-		ROWTERMINATOR = '0x0A', -- Should check in Notepad++
-		FIRSTROW = 2,
-		FORMAT = 'CSV' -- Needed to remove the quotes
+		ROWTERMINATOR = '\n',
+		FIRSTROW = 2
 );
 
 BULK INSERT bronze.boon_log
 FROM '[Input Path]'
 WITH (
 		FIELDTERMINATOR = ',',
-		ROWTERMINATOR = '0x0A', -- Should check in Notepad++
-		FIRSTROW = 2,
-		FORMAT = 'CSV' -- Needed to remove the quotes
+		ROWTERMINATOR = '\n',
+		FIRSTROW = 2
 );
